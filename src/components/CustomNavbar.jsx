80655ec1d5f83ee056d238bcc8f74b5d8e2431fb
IreeRodriguez <ire208@gmail.com>
2018-02-26 13:02:46 -0300

    arreglo de navbar

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -41,14 +41,6 @@ export default class CustomNavbar extends Component {
             </Form>
           </Navbar.Form>
         </Navbar.Collapse>
-        <Row>
-          <Nav>
-            <NavItem eventKey={1} componentClass={Link} href="/" to="/" > New Arrivals </NavItem>
-            <NavItem eventKey={2} componentClass={Link} href="/" to="/About" > Tech </NavItem>
-            <NavItem eventKey={3} componentClass={Link} href="/" to="/News" > Gifts </NavItem>
-            <NavItem eventKey={3} componentClass={Link} href="/" to="/News" > Clothes </NavItem>
-          </Nav>
-        </Row>
       </Navbar>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,13 +39,24 @@ export default class Home extends Component {
   render() {
     const {isLoading, category} = this.state;
     return (
-        <Jumbotron>
-          <h2>Wellcome to CodeLive</h2>
-          <p>This is how to build a website with react react router and react bootstrap</p>
-          <Link to="/about">
-            <Button bsStyle="primary" > About</Button>
-          </Link>
-        </Jumbotron>
+      <div className={`content ${isLoading ? 'is-loading' : ''}`}>
+        {
+                            !isLoading && category.length > 0 ? category.map(cat => {
+                                const {categories, id} = cat;
+                                return <div key={id}>
+                                    <h6>{categories}</h6>
+                                    <p>{id}</p>
+                                </div>
+                            }) : null
+                        }
+        </div>
+        // <Jumbotron>
+        //   <h2>Wellcome to CodeLive</h2>
+        //   <p>This is how to build a website with react react router and react bootstrap</p>
+        //   <Link to="/about">
+        //     <Button bsStyle="primary" > About</Button>
+        //   </Link>
+        // </Jumbotron>
       // <Col md={8}>
       // </Col>
       // <Grid>
@@ -60,24 +71,6 @@ export default class Home extends Component {
                             }) : null
                         }
         </div> */
-        /* <Row className="show-grid text-center">
-          <Col xs={12} sm={4} className="person-wrapper" >
-            <Image src="https://raw.githubusercontent.com/MyNameIsURL/react-bootstrap-website/master/public/assets/person-1.jpg" circle className="profile-pic" />
-            <h3>Frank</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt expedita voluptatum, quaerat minus veniam nam ex veritatis, quis distinctio ad velit laborum! Rem iste quae laborum cumque eaque dolorem id?</p>
-          </Col>
-          <Col xs={12} sm={4} className="person-wrapper" >
-            <Image src="https://raw.githubusercontent.com/MyNameIsURL/react-bootstrap-website/master/public/assets/person-1.jpg" circle className="profile-pic" />
-            <h3>Frank</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt expedita voluptatum, quaerat minus veniam nam ex veritatis, quis distinctio ad velit laborum! Rem iste quae laborum cumque eaque dolorem id?</p>
-          </Col>
-          <Col xs={12} sm={4} className="person-wrapper" >
-            <Image src="https://raw.githubusercontent.com/MyNameIsURL/react-bootstrap-website/master/public/assets/person-1.jpg" circle className="profile-pic" />
-            <h3>Frank</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt expedita voluptatum, quaerat minus veniam nam ex veritatis, quis distinctio ad velit laborum! Rem iste quae laborum cumque eaque dolorem id?</p>
-          </Col>
-        </Row> */
-      // </Grid>
     );
   }
 }
